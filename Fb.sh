e3faaf75df1012b3cafe101f39e65c2506266fc2
FiqraID <51190920+FiqraID@users.noreply.github.com>
2019-06-29 19:23:48 +0700

    Add files via upload

---
+++ Fb.sh
@@ -0,0 +1,146 @@
+blue='\033[34;1m' #biru
+green='\033[32;1m' #ijo
+purple='\033[35;1m' #purple
+cyan='\033[36;1m' #cyan
+red='\033[31;1m' #merah
+white='\033[37;1m' #putih
+yellow='\033[33;1m' #kuning
+orange='\033[1;38;5;208m' #Orange
+clear
+cowsay -f ghostbusters "FACEBOOK-CRACKERS" | lolcat
+echo $green"[•]¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦[•]"
+echo $red"[•]|||||||||||||||"$purple"FACEBOOK-CRACKERS"$red"|||||||||||||||[•]"
+echo $green"[•]¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦[•]"
+echo
+
+echo $purple"[¤]----------------------------------------[¤]"
+echo $blue   "¦  author   : Fiqra ID                     ¦ "
+echo $blue   "¦  team     : BlackMaskCyberTeam (B-MCT)   ¦ "
+echo $blue   "¦  fb       : Fiqra ID                     ¦ "
+echo $blue   "¦  ig       : @Fiqra_thanvans              ¦ "
+echo $purple"[¤]----------------------------------------[¤]"
+echo
+echo $orange"[+]==========================================[+]"
+echo $red   " |"$green"1.DARK-FB                               |"$cyan"C"$red" |"
+echo $red   " |"$green"2.FB-HACK                               |"$blue"R"$red" |"
+echo $red   " |"$green"3.BRUTEFORCE                            |"$yellow"A"$red" |"
+echo $red   " |"$green"4.YAHOO-CLONING                         |"$purple"C"$red" |"
+echo $red   " |"$green"5.MBF                                   |"$cyan"K"$red" |"
+echo $red   " |"$green"6.REPORT-FACEBOOK                       |"$purple"E"$red" |"
+echo $red   " |"$green"7.BOT KOMENT-FB                         |"$blue"R"$red" |"
+echo $red   " |"$green"8.INFORMATION-FB                        |"$yellow"S"$red" |"
+echo $red   " |"$green"9.INSTALL BAHAN²                        |"$purple"-"$red" |"
+echo $red   " |"$green"10.EXIT/KELUAR                          |"$cyan"-"$red" |"
+echo $orange"[+]==========================================[+]"
+echo $purple"┌==="$blue"["$cyan"FACEBOOK-HACK"$blua"]"$red"======"$yellow"["$cyan""MASUKAN PILIHAN MU""$purple"]"
+echo $me"¦"
+read -p"└──# " hack
+
+if [ $hack = 1 ] || [ $hack = 1 ]
+then
+clear
+figlet -f slant "bentar" | lolcat
+git clone https://github.com/KANG-BADRU/KANGHACK
+cd KANGHACK
+python2 KANGHACKDARK.py
+fi
+
+if [ $hack = 2 ] || [ $hack = 2 ]
+then
+clear
+figlet -f slant "bentar" | lolcat
+git clone https://github.com/FR13ND8/fb-hack
+pip2 install requests mechanize
+cd fb-hack
+python2 fb-hack
+fi
+
+if [ $hack = 3 ] || [ $hack = 3 ]
+then
+clear
+figlet -f slant "bentar" | lolcat
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+
+if [ $hack = 4 ] || [ $hack = 4 ]
+then
+clear
+figlet -f slant "bentar" | lolcat
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+python2 cloning.py
+fi
+
+if [ $hack = 5 ] || [ $hack = 5 ]
+then
+clear
+figlet -f slant "bentar" | lolcat
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $hack = 6 ] || [ $hack = 6 ]
+then
+clear
+figlet -f slant "bentar" | lolcat
+pkg install zip
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $hack = 7 ] || [ $hack = 7 ]
+then
+clear
+figlet -f slant "bentar" | lolcat
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $hack = 8 ] || [ $hack = 8 ]
+then
+clear
+figlet -f slant "bentar" | lolcat
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $hack = 9  ] || [ $hack = 9 ]
+then
+clear
+pkg install python2
+pkg install git
+pkg install ruby
+gem install lolcat
+pkg install toilet figlet
+pkg install bc
+pkg install zip
+pip2 install requests
+pip2 install mechanize
+pkg install wget
+pkg install clang
+pkg install nano
+pkg install tor
+pkg install termux-api
+pkg install cowsay
+pkg install sh
+fi
+
+if [ $hack = 10 ] || [ $hack = 10 ]
+then
+clear
+echo "TERIMA KASIH TELAH BERKUNJUNG" | lolcat
+echo "KLAU ADA YG EROR TINGGAL HUBUNGI AJA SYA" | lolcat
+echo "fb : Fiqra ID" | lolcat
+echo "ig : @Fiqra_thanvans" | lolcat
+echo "sampai jumpa :v" | lolcat
+echo "JANGAN BANYAK BACOT_-" | lolcat
+exit
+fi
